4d54e799991f3fd7ff8c97bdef2a4dd99982a87f
nevzat <nevzatseferoglu@gmail.com>
2020-12-19 13:40:57 +0300

    edit on proje template, transerfer test file to interior project side

---
+++ src/keenon/store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+        #Leave as empty string for base url
+    path('', views.home, name="home"),
+    path('account_details/', views.account_details, name = "account_details"),
+	path('store/', views.store, name="store"),
+	path('account/', views.account, name="account"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+]
