ecef5fb05d20cbb7b1dae9a08f4cd892c254ecc8
nevzat <nevzatseferoglu@gmail.com>
2020-12-27 17:26:55 +0300

    last tests and json converter

--- src/keenon/store/tests/test_urls.py
+++ src/keenon/store/tests/test_urls.py
@@ -1,47 +1,25 @@
 
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from store.views import account_details, product_details, home 
-from store.views import account, store, cart, checkout
+from store.views import home, store, cart, checkout
 
 # SimpleTestCase - not database related isues
+
 class TestUrls(SimpleTestCase):
-
-    def test_account_details_url_is_resolves(self):
-        url = reverse('account_details')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, account_details)
-
-    '''
-    given en error
-    def test_product_details_url_is_resolves(self):
-        url = reverse('product_details')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, product_details)
-    '''
 
     def test_home_url_is_resolves(self):
         url = reverse('home')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, home)
-
-    def test_account_url_is_resolves(self):
-        url = reverse('account')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, account)
     
     def test_store_url_is_resolves(self):
         url = reverse('store')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, store)
 
     def test_cart_url_is_resolves(self):
         url = reverse('cart')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, cart)
     
     def test_checkout_url_is_resolves(self):
         url = reverse('checkout')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, checkout)
 
--- src/keenon/store/tests/test_views.py
+++ src/keenon/store/tests/test_views.py
@@ -1,46 +1,33 @@
 
 from django.test import TestCase, Client
 from django.urls import reverse
-from store.models import Customer, Product, Order, OrderItem, ShippingAddress
+from store.models import Address, Product, Order, OrderItem, ShippingAddress
 import json
 
-
-# path url name ile html file in ismini ayni yapsakdik
-# daha guzel olurmus
 
 class TestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
         self.home_url = reverse('home')
-        self.account_details_url = reverse('account_details')
-        #self.product_details_url = reverse('product_details')
-        self.account_url = reverse('account')
         self.store_url = reverse('store')
         self.cart_url = reverse('cart')
-        self. checkout_url = reverse('checkout')
+        self.checkout_url = reverse('checkout')
+        #self.product_details_url = reverse('product_details')
 
-    def test_account_details_GET(self):
-        response = self.client.get(self.account_details_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'store/account-details.html')
-    
+
     '''
-    erroneous test
+    Error occureed
     def test_product_details_GET(self):
         response = self.client.get(self.product_details_url)
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'store/product_details_url.html')
+        self.assertTemplateUsed(response, 'store/store/product-details.html')
     '''
+
     def test_home_GET(self):
         response = self.client.get(self.home_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'store/index.html')
-
-    def test_account_url_is_resolves(self):
-        response = self.client.get(self.account_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'store/account.html')
 
     def test_store_GET(self):
         response = self.client.get(self.store_url)
@@ -52,7 +39,7 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'store/cart.html')
 
-    def test_cart_GET(self):
+    def test_checkout_GET(self):
         response = self.client.get(self.checkout_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'store/checkout.html')
