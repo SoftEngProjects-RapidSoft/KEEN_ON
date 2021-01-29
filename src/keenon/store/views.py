abe080804a9a8c6366acb33ec97468ecf47dba01
BaranHasanBozduman <baranhbozduman@gmail.com>
2021-01-29 17:54:34 +0300

    address check message before checkout

--- src/keenon/store/views.py
+++ src/keenon/store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Product, Order
+from account.views import account_details
 from account.models import Customer
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib import messages
 import json
 import datetime
 from .models import *
@@ -17,111 +19,7 @@ from django.core.mail import EmailMessage
 from django.shortcuts import redirect
 from django.template.loader import get_template
 # Create your views here.
-'''
-def updateItem(request):
 
-				data = json.loads(request.body)
-
-				productId = data['productId']
-
-				action = data['action']
-
-				print('Action:', action)
-
-				print('Product:', productId)
-
- 
-
-				customer = request.user.customer
-
-				product = Product.objects.get(id=productId)
-
-				order, created = Order.objects.get_or_create(customer=customer, complete=False)
-
- 
-
-				orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
-
- 
-
-				if action == 'add':
-
-								orderItem.quantity = (orderItem.quantity + 1)
-
-				elif action == 'remove':
-
-								orderItem.quantity = (orderItem.quantity - 1)
-
- 
-
-				orderItem.save()
-
- 
-
-				if orderItem.quantity <= 0:
-
-								orderItem.delete()
-
- 
-
-				return JsonResponse('Item was added', safe=False)
-
- 
-
-def processOrder(request):
-
-				transaction_id = datetime.datetime.now().timestamp()
-
-				data = json.loads(request.body)
-
- 
-
-				if request.user.is_authenticated:
-
-								customer = request.user.customer
-
-								order, created = Order.objects.get_or_create(customer=customer, complete=False)
-
-								total = float(data['form']['total'])
-
-								order.transaction_id = transaction_id
-
- 
-
-								if total == order.get_cart_total:
-
-												order.complete = True
-
-								order.save()
-
- 
-
-								if order.shipping == True:
-
-												ShippingAddress.objects.create(
-
-												customer=customer,
-
-												order=order,
-
-												address=data['shipping']['address'],
-
-												city=data['shipping']['city'],
-
-												state=data['shipping']['state'],
-
-												zipcode=data['shipping']['zipcode'],
-
-												)
-
-				else:
-
-								print('User is not logged in')
-
- 
-
-				return JsonResponse('Payment submitted..', safe=False)
-'''
 def product_details(request):
 	products = Product.objects.all()
 	context = {'products':products}
@@ -171,6 +69,11 @@ def checkout(request):
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
 		cartItems = order.get_cart_items
+		if (request.user.address.country==None):
+			messages.warning(request, 'You need to update your address information from Account Page->Edit User Information')
+			context = {'messages':messages,'items':items, 'order':order, 'cartItems':cartItems}
+			return redirect('cart')
+
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
