bce022a4779db7b3e4d419a37b707f9a3677dedd
alibhr <ali.bahar12389@gmail.com>
2021-01-31 14:13:46 +0300

    reset password is added

--- src/keenon/account/urls.py
+++ src/keenon/account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = 'account'
 
@@ -11,4 +13,19 @@ urlpatterns = [
 	path('register/', views.registerPage, name='register'),
 	path('login/', views.loginPage, name='login'),
 	path('logout/', views.logoutUser, name='logout'),
+
+	path('reset_password/', auth_views.PasswordResetView.as_view(
+		email_template_name="account/password_reset_email.html",
+		success_url=reverse_lazy("account:password_reset_done"),
+		template_name = "account/password_reset.html"), name="reset_passwod"),
+
+	path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+		template_name="account/password_reset_sent.html"), name="password_reset_done"),
+
+	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+		success_url=reverse_lazy("account:password_reset_complete"),
+		template_name="account/password_reset_form.html"), name="password_reset_confirm"),
+	
+	path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+		template_name="account/password_reset_done.html"), name="password_reset_complete"),
 ]
